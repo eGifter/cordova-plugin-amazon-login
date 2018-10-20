4d9bb653762f63f843e856d01c35109c4dba7164
Bikram Ghosh Dastidar <cypherix93@users.noreply.github.com>
2018-10-20 13:55:53 -0400

    Update after_prepare.js

--- scripts/android/after_prepare.js
+++ scripts/android/after_prepare.js
@@ -2,12 +2,12 @@
 'use strict';
 var fs = require('fs-extra');
 
-var target = "debug";
-if (process.env.TARGET) {
-  target = process.env.TARGET;
+var env = "Development";
+if (process.env.NODE_ENV) {
+  env = process.env.NODE_ENV;
 }
 
 var configobj = JSON.parse(fs.readFileSync('config/project.json', 'utf8'));
 
 var filename = "platforms/android/app/src/main/assets/api_key.txt";
-fs.writeFileSync(filename, configobj[target].AMAZON_API_KEY, 'utf8');
+fs.writeFileSync(filename, configobj[env].AMAZON_API_KEY, 'utf8');
